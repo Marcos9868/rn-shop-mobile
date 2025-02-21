9538ca7e2628ed708a57978eea78ac7c506a757a
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-02-21 17:00:38 -0300

    Feat: add footer navigation
    Add Shop cart and order menus on footer

--- src/app/(shop)/_layout.tsx
+++ src/app/(shop)/_layout.tsx
@@ -1,21 +1,53 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { SafeAreaView, StyleSheet } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import { FontAwesome } from '@expo/vector-icons'
+
+function TabBarIcon(
+  props: {
+    name: React.ComponentProps<typeof FontAwesome>['name'],
+    color: string
+  }
+) {
+  return <FontAwesome size={24} {...props} style={{ color: '#1BC464' }} />
+}
 
 const TabsLayout = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.safeArea}>
       <Tabs screenOptions={{
         tabBarActiveTintColor: '#1BC464',
-        tabBarInactiveTintColor: 'gray'
+        tabBarInactiveTintColor: 'gray',
+        tabBarLabelStyle: { fontSize: 16 },
+        tabBarStyle: {
+          borderBottomLeftRadius: 20,
+          borderBottomRightRadius: 20,
+          height: 70,
+          padding: 4
+        },
+        headerShown: false
       }}>
         <Tabs.Screen name='index' options={{
-          headerShown: false
+          title: 'shop',
+          tabBarIcon(props) {
+              return <TabBarIcon {...props} name='shopping-cart' />
+          }
         }} /> 
-        <Tabs.Screen name='orders' options={{}} /> 
+        <Tabs.Screen name='orders' options={{
+          title: 'orders',
+          tabBarIcon(props) {
+            return <TabBarIcon {...props} name='book' />
+          }
+        }} /> 
       </Tabs>
     </SafeAreaView>
   )
 }
 
-export default TabsLayout
+export default TabsLayout;
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1
+  }
+});
